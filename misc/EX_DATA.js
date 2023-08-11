a09122720b6eeb59d172ba2de23b2a811da4bb01
benaber1990 <benaber1990@gmail.com>
2023-08-11 17:36:07 +0100

    first

---
+++ misc/EX_DATA.js
@@ -0,0 +1,61 @@
+let outputNumber = 0;
+
+let enteredNumber = 0;
+
+if (enteredNumber === 01) {
+  outputNumber = 4;
+} else if (enteredNumber == "	0001	") {
+  outputNumber = "MIN:	1	SEC:	01	";
+} else if (enteredNumber == "	0002	") {
+  outputNumber = "MIN:	1	SEC:	02	";
+} else if (enteredNumber == "	0003	") {
+  outputNumber = "MIN:	1	SEC:	03	";
+} else if (enteredNumber == "	0004	") {
+  outputNumber = "MIN:	1	SEC:	04	";
+} else if (enteredNumber == "	0005	") {
+  outputNumber = "MIN:	1	SEC:	05	";
+} else if (enteredNumber == "	0006	") {
+  outputNumber = "MIN:	1	SEC:	06	";
+} else if (enteredNumber == "	0007	") {
+  outputNumber = "MIN:	1	SEC:	07	";
+} else if (enteredNumber == "	0008	") {
+  outputNumber = "MIN:	1	SEC:	08	";
+} else if (enteredNumber == "	0009	") {
+  outputNumber = "MIN:	1	SEC:	09	";
+} else if (enteredNumber == "	0010	") {
+  outputNumber = "MIN:	1	SEC:	10	";
+} else if (enteredNumber == "	0011	") {
+  outputNumber = "MIN:	1	SEC:	11	";
+} else if (enteredNumber == "	0012	") {
+  outputNumber = "MIN:	1	SEC:	12	";
+} else if (enteredNumber == "	0013	") {
+  outputNumber = "MIN:	1	SEC:	13	";
+} else if (enteredNumber == "	0014	") {
+  outputNumber = "MIN:	1	SEC:	14	";
+} else if (enteredNumber == "	0015	") {
+  outputNumber = "MIN:	1	SEC:	15	";
+} else if (enteredNumber == "	0016	") {
+  outputNumber = "MIN:	1	SEC:	16	";
+} else if (enteredNumber == "	0017	") {
+  outputNumber = "MIN:	1	SEC:	17	";
+} else if (enteredNumber == "	0018	") {
+  outputNumber = "MIN:	1	SEC:	18	";
+} else if (enteredNumber == "	0019	") {
+  outputNumber = "MIN:	1	SEC:	19	";
+} else if (enteredNumber == "	0020	") {
+  outputNumber = "MIN:	1	SEC:	20	";
+} else if (enteredNumber == "	0021	") {
+  outputNumber = "MIN:	1	SEC:	21	";
+} else if (enteredNumber == "	0022	") {
+  outputNumber = "MIN:	1	SEC:	22	";
+} else if (enteredNumber == "	0023	") {
+  outputNumber = "MIN:	1	SEC:	23	";
+} else if (enteredNumber == "	0024	") {
+  outputNumber = "MIN:	1	SEC:	24	";
+} else if (enteredNumber == "	0025	") {
+  outputNumber = "MIN:	1	SEC:	25	";
+} else if (enteredNumber == "	0026	") {
+  outputNumber = "MIN:	1	SEC:	26	";
+} else if (enteredNumber == "	0027	") {
+  outputNumber = "MIN:	1	SEC:	27	";
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,145 @@
-import * as React from "react"
+import React, { useState } from "react";
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+  fontFamily: "poppins",
+  justifyContent: "center",
+  display: "flex",
+  // backgroundColor: "red",
+};
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
+const cardStyle = {
+  backgroundColor: "green",
+  width: 400,
+  height: 250,
+  paddingRight: 20,
+  paddingLeft: 20,
+  paddingTop: 30,
+  paddingBottom: 30,
+  borderRadius: 20,
   marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+};
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+const submitButtonStyle = {
+  backgroundColor: "yellow",
+  paddingTop: 1,
+  paddingBottom: 1,
+  paddingRight: 5,
+  paddingLeft: 5,
+  borderRadius: 6,
+  marginTop: 20,
+  width: "50%",
+};
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const button2Style = {
+  backgroundColor: "yellow",
+  paddingTop: 1,
+  paddingBottom: 1,
+  paddingRight: 5,
+  paddingLeft: 5,
+  borderRadius: 6,
+  marginTop: 20,
+  // width: "50%",
+};
 
 const IndexPage = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [hasSubmitted, setHasSubmitted] = useState(true);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  // const [hasSubmitted, setHasSubmitted] = useState(false);
+
+  // let enteredNumber = "a";
+  // let outputNumber = " 0001 ";
+
+  let demoText = "";
+
+  if (inputValue.length >= 1) {
+    demoText = "Yessss";
+  } else {
+    demoText = "Noooo";
+  }
+
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+      {hasSubmitted ? (
+        <div style={cardStyle}>
+          <div style={{ color: "white", fontSize: 32, textAlign: "center" }}>
+            Enter your ticket to see your time
+          </div>
+          <div style={{ justifyContent: "center", display: "flex" }}>
+            <label>
+              <input
+                type="text"
+                value={inputValue}
+                onChange={handleChange}
+                style={{
+                  height: 50,
+                  width: 240,
+                  marginTop: 10,
+                  paddingLeft: 25,
+                }}
+              />
+            </label>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div
+              onClick={() => setHasSubmitted((prevState) => !prevState)}
+              style={submitButtonStyle}
+            >
+              <h3
+                style={{
+                  textAlign: "center",
+                  alignSelf: "center",
+                }}
               >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+                Submit
+              </h3>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div style={cardStyle}>
+            <div style={{ color: "white", fontSize: 32, textAlign: "center" }}>
+              Your time is:
+            </div>
+            <div
+              style={{
+                color: "white",
+                fontSize: 42,
+                fontWeight: "600",
+                textAlign: "center",
+                marginTop: 10,
+              }}
+            >
+              {demoText}
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <div
+                onClick={() => setHasSubmitted((prevState) => !prevState)}
+                style={button2Style}
+              >
+                <h3
+                  style={{
+                    textAlign: "center",
+                    alignSelf: "center",
+                  }}
+                >
+                  Check another ticket
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home Page</title>;
